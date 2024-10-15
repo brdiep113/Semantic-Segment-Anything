eb4f9934c1d87dc0e587d4f5ae3a7f67cdac1a30
Brian <brdiep113@gmail.com>
2024-10-15 16:47:34 -0700

    Add scripts for running jobs on slurm

---
+++ slurm_scripts/auto_annotate_flickr.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:p100:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=36:00:00    
+#SBATCH --mail-user=<brdiep@mail.ubc.ca>
+#SBATCH --mail-type=ALL
+
+cd ~/projects/def-lingjzhu/brdiep/Semantic-Segment-Anything
+
+# Set up modules/virtual environment
+module purge
+module load StdEnv/2023 python/3.11 scipy-stack gcc/12.3 arrow/14 cuda
+source ~/std23/bin/activate
+
+python scripts/main_ssa_engine.py --data_dir=~/scratch/Flickr8k_dataset --out_dir=~/scratch/Flickr8k_annotated --world_size=8 --save_img --sam --ckpt_path=ckp/sam_vit_h_4b8939.pth
+
