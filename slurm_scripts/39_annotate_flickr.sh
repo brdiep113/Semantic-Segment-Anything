c33df760e8e4a885a0dee7a9848787644193c04a
Brian <brdiep113@gmail.com>
2024-10-22 16:21:46 -0700

    New script

---
+++ slurm_scripts/39_annotate_flickr.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --mem=64G
+#SBATCH --gpus-per-node=v100l:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=8:00:00    
+#SBATCH --mail-user=<brdiep@mail.ubc.ca>
+#SBATCH --mail-type=ALL
+
+cd ~/projects/def-lingjzhu/brdiep/Semantic-Segment-Anything
+
+# Set up modules/virtual environment
+module purge
+module load StdEnv/2020 python/3.9 gcc cuda opencv
+source ~/py39/bin/activate
+
+python scripts/main_ssa_engine.py --data_dir=~/scratch/Flickr8k_dataset --out_dir=~/scratch/Flickr8k_annotated --world_size=8 --save_img --sam --ckpt_path=ckp/sam_vit_h_4b8939.pth
+
