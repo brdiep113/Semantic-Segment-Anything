ea4e121c3153819d6080eb5891576ed92150f94b
Brian <brdiep113@gmail.com>
2024-10-22 13:14:09 -0700

    Move to python 38

--- slurm_scripts/auto_annotate_flickr.sh
+++ slurm_scripts/auto_annotate_flickr.sh
@@ -12,7 +12,7 @@ cd ~/projects/def-lingjzhu/brdiep/Semantic-Segment-Anything
 # Set up modules/virtual environment
 module --force purge
 module load StdEnv/2020 python/3.8 scipy-stack/2020a gcc cuda opencv
-source ~/py37/bin/activate
+source ~/py38/bin/activate
 
 python scripts/main_ssa_engine.py --data_dir=~/scratch/Flickr8k_dataset --out_dir=~/scratch/Flickr8k_annotated --world_size=8 --save_img --sam --ckpt_path=ckp/sam_vit_h_4b8939.pth
 
