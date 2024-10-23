0bd1553a4fc7ba2070c69e760621900c075023fd
Brian <brdiep113@gmail.com>
2024-10-22 21:36:18 -0700

    install mmcv from source

--- slurm_scripts/310_annotate_flickr.sh
+++ slurm_scripts/310_annotate_flickr.sh
@@ -11,9 +11,9 @@ cd ~/projects/def-lingjzhu/brdiep/Semantic-Segment-Anything
 
 # Set up modules/virtual environment
 module purge
-module load StdEnv/2023 python/3.10 scipy-stack gcc cuda opencv
+module load StdEnv/2020 python/3.10 scipy-stack gcc cuda/11.0 opencv
 source ~/py310/bin/activate
 
-pip install --upgrade mmcv==2.0.0
+pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.10.1/index.html
 python scripts/main_ssa_engine.py --data_dir=~/scratch/Flickr8k_dataset --out_dir=~/scratch/Flickr8k_annotated --world_size=8 --save_img --sam --ckpt_path=ckp/sam_vit_h_4b8939.pth
 
